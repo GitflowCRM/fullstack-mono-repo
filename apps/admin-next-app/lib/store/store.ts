f815773c9f248d235aa7b27fffb835687c3fdafd
muneer-gitflow <145769428+muneer-gitflow@users.noreply.github.com>
2025-01-14 00:36:14 +0400

    added redux and inital page

---
+++ apps/admin-next-app/lib/store/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import appReducer from './reducers/app.slice';
+
+export const store = configureStore({
+  reducer: {
+    appState: appReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
